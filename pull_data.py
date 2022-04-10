8e14731752d317e717d80d3b6d68c2c72da0c390
comnk <vhsiao36@gmail.com>
2022-04-10 16:25:57 -0700

    Updates

--- pull_data.py
+++ pull_data.py
@@ -2,14 +2,14 @@ import yfinance as yf
 from csv import *
 from datetime import date
 
-stocks = ['FB']
+tickers = ['msft', 'aapl', 'twtr', 'intc', 'tsm', 'goog', 'amzn', 'fb', 'nvda']
 
-data = yf.Ticker("FB")
-end_date = date.today().strftime("%Y-%m-%d")
-start_date = date.today().strftime("-%m-%d")
-yearly = data.history(start=str(int(date.today().strftime("%Y")) - 1)+start_date, end=end_date)
-yearly.insert(loc=0, column="Stock Symbol", value='FB', allow_duplicates=True)
-print(yearly.head())
-print(type(yearly))
-
-yearly.to_csv('data.csv')
+for ticker in tickers:
+    data = yf.Ticker(ticker)
+    end_date = date.today().strftime("%Y-%m-%d")
+    start_date = date.today().strftime("-%m-%d")
+    yearly = data.history(start=str(int(date.today().strftime("%Y")) - 1)+start_date, end=end_date)
+    yearly.insert(loc=0, column="Stock Symbol", value=ticker, allow_duplicates=True)
+    print(yearly.head())
+    print(type(yearly))
+    yearly.to_csv(ticker + '.csv')
