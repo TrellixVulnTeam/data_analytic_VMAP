b7b3c68aedaf0ac0134af6180904ad3a3a1e6e96
comnk <vhsiao36@gmail.com>
2022-04-09 18:02:26 -0700

    Updates

--- pull_data.py
+++ pull_data.py
@@ -2,10 +2,13 @@ import yfinance as yf
 from csv import *
 from datetime import date
 
+stocks = ['FB']
+
 data = yf.Ticker("FB")
 end_date = date.today().strftime("%Y-%m-%d")
 start_date = date.today().strftime("-%m-%d")
 yearly = data.history(start=str(int(date.today().strftime("%Y")) - 1)+start_date, end=end_date)
+yearly.insert(loc=0, column="Stock Symbol", value='FB', allow_duplicates=True)
 print(yearly.head())
 print(type(yearly))
 
